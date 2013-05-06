b6b95f3840ca1c988e41c84e8a27ebb73f4bcab5
joanhey <joanhey@kumbiaphp.com>
2013-05-06 12:55:04 +0300

    Update ActRow.php

--- ActRecord/ActRow.php
+++ ActRecord/ActRow.php
@@ -4,7 +4,7 @@
  *
  * @category   Kumbia
  * @package    ActRecord
- * @copyright  Copyright (c) 2005-2012 KumbiaPHP Team (http://www.kumbiaphp.com)
+ * @copyright  Copyright (c) 2005-2013 KumbiaPHP Team (http://www.kumbiaphp.com)
  * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
  */
 
