33610be85d22a68450e98e75bb1f8db7168ab0d5
joanhey <joanhey@kumbiaphp.com>
2013-05-06 13:01:06 +0300

    Create ActRecord.php

---
+++ ActRecord/ActRecord.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * KumbiaPHP web & app Framework
+ *
+ * @category   Kumbia
+ * @package    ActRecord
+ * @copyright  Copyright (c) 2005-2013 KumbiaPHP Team (http://www.kumbiaphp.com)
+ * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
+ */
+
+/**
+ * Clase para recorrer los resultados de la consulta
+ *
+ * @category   Kumbia
+ * @package    ActRecord
+ */
+
+//NAMESPACE
+
+class ActRecord
+{
+  
+
+  
+}
