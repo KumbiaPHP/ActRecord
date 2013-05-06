be10467624fef474d5f416ef4321fb4b9dffbbe4
joanhey <joanhey@kumbiaphp.com>
2013-05-06 12:54:39 +0300

    Update ActRow.php

--- ActRecord/ActRow.php
+++ ActRecord/ActRow.php
@@ -2,13 +2,6 @@
 /**
  * KumbiaPHP web & app Framework
  *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://wiki.kumbiaphp.com/Licencia
- *
  * @category   Kumbia
  * @package    ActRecord
  * @copyright  Copyright (c) 2005-2012 KumbiaPHP Team (http://www.kumbiaphp.com)
